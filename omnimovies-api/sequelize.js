30d6d8f1952832d47265e924d049576b7c3a2bb4
Juan Carlos Hernandez Ramos <contacto@juancramos.com>
2019-12-05 17:07:00 -0500

    set backend deploy

--- omnimovies-api/sequelize.js
+++ omnimovies-api/sequelize.js
@@ -1,14 +1,35 @@
 const Sequelize = require('sequelize');
 
 module.exports = function (app) {
-  const connectionString = app.get('sqlite');
-  const sequelize = new Sequelize(connectionString, {
-    dialect: 'sqlite',
+  const connectionString = app.get('mysql');
+  
+  let sequelize = null;
+  let sequelizeParams = {
+    dialect: 'mysql',
     logging: false,
     define: {
       freezeTableName: true
     }
-  });
+  };
+  if (process.env.PORT) {
+    const vars = connectionString.split(';');
+    const database = vars[0].split('=')[1];
+    const source = vars[1].split('=')[1];
+    const hostName = source.split(':');
+    const usr = vars[2].split('=')[1];
+    const psw = vars[3].split('=')[1];
+
+    Object.assign(sequelizeParams, {
+      host: hostName[0],
+      port: hostName[1]
+    });
+
+    sequelize = new Sequelize(database, usr, psw, sequelizeParams);
+  }
+  else {
+    sequelize = new Sequelize(connectionString, sequelizeParams);
+  }
+
   const oldSetup = app.setup;
 
   app.set('sequelizeClient', sequelize);
