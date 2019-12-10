5c070cb11c94e5a92e5d71f18905d613b5524879
Juan Carlos Hernandez Ramos <contacto@juancramos.com>
2019-12-10 01:36:50 -0500

    home ready

--- omnimovies-client/src/components/home/List.vue
+++ omnimovies-client/src/components/home/List.vue
@@ -1,67 +1,76 @@
 <template>
-  <section>
-    <b-table
-      :data="data"
-      :loading="loading"
-      paginated
-      backend-pagination
-      :total="total"
-      :per-page="perPage"
-      @page-change="onPageChange"
-      aria-next-label="Next page"
-      aria-previous-label="Previous page"
-      aria-page-label="Page"
-      aria-current-label="Current page"
-      backend-sorting
-      :default-sort-direction="defaultSortOrder"
-      :default-sort="[sortField, sortOrder]"
-      @sort="onSort"
-    >
-      <template slot-scope="props">
-        <b-table-column field="original_title" label="Title" sortable>
-          {{ props.row.original_title }}
-        </b-table-column>
+  <div class="card">
+    <div class="card-content">
+      <b-table
+        :data="data"
+        :loading="loading"
+        paginated
+        backend-pagination
+        :total="total"
+        :per-page="perPage"
+        @page-change="onPageChange"
+        aria-next-label="Next page"
+        aria-previous-label="Previous page"
+        aria-page-label="Page"
+        aria-current-label="Current page"
+        backend-sorting
+        :default-sort-direction="defaultSortOrder"
+        :default-sort="[sortField, sortOrder]"
+        @sort="onSort"
+        icon-pack="fas"
+      >
+        <template slot-scope="props">
+          <b-table-column field="original_title" label="Title" sortable>
+            {{ props.row.original_title }}
+          </b-table-column>
 
-        <b-table-column
-          field="vote_average"
-          label="Vote Average"
-          numeric
-          sortable
-        >
-          <span class="tag" :class="type(props.row.vote_average)">
-            {{ props.row.vote_average }}
-          </span>
-        </b-table-column>
+          <b-table-column
+            field="vote_average"
+            label="Vote Average"
+            numeric
+            sortable
+          >
+            <span class="tag" :class="type(props.row.vote_average)">
+              {{ props.row.vote_average }}
+            </span>
+          </b-table-column>
 
-        <b-table-column field="vote_count" label="Vote Count" numeric sortable>
-          {{ props.row.vote_count }}
-        </b-table-column>
+          <b-table-column
+            field="vote_count"
+            label="Vote Count"
+            numeric
+            sortable
+          >
+            {{ props.row.vote_count }}
+          </b-table-column>
 
-        <b-table-column
-          field="release_date"
-          label="Release Date"
-          sortable
-          centered
-        >
-          {{
-            props.row.release_date
-              ? new Date(props.row.release_date).toLocaleDateString()
-              : ""
-          }}
-        </b-table-column>
+          <b-table-column
+            field="release_date"
+            label="Release Date"
+            sortable
+            centered
+          >
+            {{
+              props.row.release_date
+                ? new Date(props.row.release_date).toLocaleDateString()
+                : ""
+            }}
+          </b-table-column>
 
-        <b-table-column label="Overview" width="500">
-          {{ props.row.overview | truncate(80) }}
-        </b-table-column>
-      </template>
-    </b-table>
-  </section>
+          <b-table-column label="Overview" width="500">
+            {{ props.row.overview | truncate(80) }}
+          </b-table-column>
+        </template>
+      </b-table>
+    </div>
+  </div>
 </template>
 
 <script>
-import axios from "axios";
-
 export default {
+  props: {
+    mainLoadAsyncData: Function
+  },
   data() {
     return {
       data: [],
@@ -75,42 +84,28 @@ export default {
     };
   },
   methods: {
-    /*
-     * Load async data
-     */
-    loadAsyncData() {
-      const params = [
-        "api_key=bb6f51bef07465653c3e553d6ab161a8",
-        "language=en-US",
-        "include_adult=false",
-        "include_video=false",
-        `sort_by=${this.sortField}.${this.sortOrder}`,
-        `page=${this.page}`
-      ].join("&");
+    async loadAsyncData() {
+      const params = {
+        sortField: this.sortField,
+        sortOrder: this.sortOrder,
+        page: this.page
+      };
 
       this.loading = true;
-      axios
-        .get(`https://api.themoviedb.org/3/discover/movie?${params}`)
-        .then(({ data }) => {
-          // api.themoviedb.org manage max 1000 pages
-          this.data = [];
-          let currentTotal = data.total_results;
-          if (data.total_results / this.perPage > 1000) {
-            currentTotal = this.perPage * 1000;
-          }
-          this.total = currentTotal;
-          data.results.forEach(item => {
-            item.release_date = item.release_date.replace(/-/g, "/");
-            this.data.push(item);
-          });
-          this.loading = false;
-        })
-        .catch(error => {
-          this.data = [];
-          this.total = 0;
-          this.loading = false;
-          throw error;
-        });
+      const { data } = await this.mainLoadAsyncData(params);
+      // api.themoviedb.org manage max 1000 pages
+      this.data = [];
+      let currentTotal = data.total_results;
+      if (data.total_results / this.perPage > 1000) {
+        currentTotal = this.perPage * 1000;
+      }
+      this.total = currentTotal;
+      data.results.forEach(item => {
+        if (!item) return;
+        item.release_date = item.release_date.replace(/-/g, "/");
+        this.data.push(item);
+      });
+      this.loading = false;
     },
     /*
      * Handle page-change event
